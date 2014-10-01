7a7c6af49c6836acecba4b802992615dbd1cd714
Mattiasjonss <puffen.jonsson@hotmail.com>
2014-10-01 12:09:30 +0200

    Calculate

---
+++ calculator.py
@@ -0,0 +1,70 @@
+Python 2.7.8 (default, Jun 30 2014, 16:03:49) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Lägg till första siffran: 12
+Lägg till andra siffran: 12
+(12, '+', 12, '=', 24)
+>>> ================================ RESTART ================================
+>>> 
+Lägg till första siffran: 12
+Lägg till andra siffran: 12
+True
+>>> ================================ RESTART ================================
+>>> 
+Lägg till första siffran: 12
+Lägg till andra siffran: 12
+(24, '=', 24)
+>>> ================================ RESTART ================================
+>>> 
+Lägg till första siffran: 15
+Lägg till andra siffran: 17
+(32, '=', 32)
+>>> ================================ RESTART ================================
+>>> 
+Lägg till första siffran: 12
+Lägg till andra siffran: 15
+27
+>>> ================================ RESTART ================================
+>>> 
+Lägg till första siffran: 35
+Lägg till andra siffran: 12
+47
+>>> ================================ RESTART ================================
+>>> 
+Lägg till första siffran: 
+>>> ================================ RESTART ================================
+>>> 
+Lägg till första siffran: 12
+Lägg till andra siffran: 4
+8
+>>> ================================ RESTART ================================
+>>> 
+Lägg till första siffran: 15
+Lägg till andra siffran: 14
+29
+1
+1
+210
+>>> ================================ RESTART ================================
+>>> 
+Lägg till första siffran: 12
+Lägg till andra siffran: 12
+num1 + num2 =
+24
+0
+1
+144
+>>> ================================ RESTART ================================
+>>> 
+Lägg till första siffran: 15
+Lägg till andra siffran: 17
+Plus
+32
+Minus
+-2
+Dividerat
+0
+Multiplicerat
+255
+>>> 
